31de9bc849e66a650fd21a44cfe48bef5fd90f36
huangxin <huangxin@nibirutech.com>
2017-06-13 19:29:54 +0800

    modify license

--- timer/safetimer_test.go
+++ timer/safetimer_test.go
@@ -28,10 +28,12 @@ func Test(t *testing.T) {
 		}
 	}()
 	go func() {
-		for {
-			s.CreateTimer(int64(rand.Int31n(25*3600*1e3)), test, []interface{}{22, 33})
+		i := 0
+		for i<50000{
+			s.CreateTimer(int64(rand.Int31n(3600*1e3)), test, []interface{}{22, 33})
 			atomic.AddInt64(&tt, 1)
 			time.Sleep(1 * time.Second)
+			i += 1
 		}
 	}()
 	go func(){
@@ -40,9 +42,11 @@ func Test(t *testing.T) {
 			logger.Info("last timer: ", atomic.LoadInt64(&tt))
 		}
 	}()
-	for {
-		s.CreateTimer(int64(rand.Int31n(25*3600*1e3)), test, []interface{}{22, 33})
+	ii := 0
+	for ii < 50000{
+		s.CreateTimer(int64(rand.Int31n(3600*1e3)), test, []interface{}{22, 33})
 		atomic.AddInt64(&tt, 1)
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
+		ii += 1
 	}
 }
