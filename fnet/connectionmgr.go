d700c0a2a9e91c3fb6ce8fe955237504e18bdc6a
tobygz <2503417117@qq.com>
2017-09-06 11:12:21 +0800

    fix multi goroutine error

--- fnet/connectionmgr.go
+++ fnet/connectionmgr.go
@@ -35,8 +35,6 @@ func (this *ConnectionMgr) Remove(conn iface.Iconnection) error {
 }
 
 func (this *ConnectionMgr) Get(sid uint32) (iface.Iconnection, error) {
-	this.conMrgLock.Lock()
-	defer this.conMrgLock.Unlock()
 	v, ok := this.connections[sid]
 	if ok {
 		delete(this.connections, sid)
@@ -47,8 +45,6 @@ func (this *ConnectionMgr) Get(sid uint32) (iface.Iconnection, error) {
 }
 
 func (this *ConnectionMgr) Len() int {
-	this.conMrgLock.Lock()
-	defer this.conMrgLock.Unlock()
 	return len(this.connections)
 }
 
