dd77685739b0e1c0ed0a66389441b697074b39db
huangxin <huangxin@nibirutech.com>
2018-07-09 14:15:53 +0800

    fix accept panic

--- fserver/server.go
+++ fserver/server.go
@@ -97,6 +97,7 @@ func (this *Server) Start() {
 			conn, err := ln.AcceptTCP()
 			if err != nil {
 				logger.Error(err)
+                                continue
 			}
 			//max client exceed
 			if this.connectionMgr.Len() >= utils.GlobalObject.MaxConn {
